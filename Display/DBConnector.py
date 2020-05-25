a104b35d8b0fb72381f19db433982e02647a6777
Ubuntu <stud@hadoop-worker-2.bvh2l1tyuofungvgtcutmu2tlc.fx.internal.cloudapp.net>
2020-05-25 19:19:19 +0000

    initial commit

--- Display/DBConnector.py
+++ Display/DBConnector.py
@@ -4,61 +4,107 @@ import time
 from pprint import pprint
 from pymongo import MongoClient
 
-DELAY_SEC = 2
-DB_NAME = 'tweet_sentiment'
-COMP_NAME = 'DHL'
+# DB_NAME = 'tweet_sentiment'
+DB_NAME = 'histogram_test'
 WORKER1_IP = '10.0.2.6'
 
 client = MongoClient(WORKER1_IP)
 db = client[DB_NAME]
 
-companies = db.list_collection_names()
-print('Companies:', companies)
-cursors = {
-    comp_name: db[comp_name].find()
-    for comp_name in companies
-}
+comp_names = db.list_collection_names()
+print('Names of companies:', comp_names)
 
+
+# Later on we'll change this to choose company
+# by choosing a button
+'''
 collection = db[COMP_NAME]
-
-shown = 0
-
 print('\n======= COUNTING =======')
 print('Cursor points to documents:', collection.count_documents({}))
-
 print('\n======= READING (and adding new) =======')
-
-latest_doc = 'nothing'
 cursor = cursors[COMP_NAME]
+'''
 
 
+# HISTORY
+# Reads collection from the beginning
+# One tweet at a time
+# Use this in historical data display
 
-# while True:
-def get_next_post(comp_name):
-    # print('\n Post shown already:', shown1)
-    print('\nGetting next post...')
-    shown1 = shown
-    cursor = cursors[comp_name]
+# Fire update_history_meta callback 
+# every HISTORY_PERIOD_SEC seconds
+HISTORY_PERIOD_SEC = 2
+
+# Cursors point to the beginning
+history_cursors = {
+    comp_name: db[comp_name].find()
+    for comp_name in comp_names
+}
+
+# Counters show how many tweets already shown
+# from this company
+history_counters = {
+    comp_name: 0
+    for comp_name in comp_names
+}
+
+def update_history(comp_name, counters, verbose=False):
+    shown = counters[comp_name]
+    if verbose:
+        print('========== Next Historical Post ==========')
+        print('Already shown:', shown)
+    cursor = history_cursors[comp_name]
     try:
-        doc = cursor.next()
+        post = cursor.next()
     except StopIteration:
-        print('No more new posts! Looking for more...', '\n')
-        # load_batch(cursor, shown)
-        cursor = collection.find().skip(shown)
-        pprint(latest_doc)
-        return latest_doc
 
-    else:
+        if verbose:
+            print('No more new posts! Looking for more...', '\n')
+        cursor = db[comp_name].find().skip(shown)
+        
+    finally:
+        post = cursor.next()
+        counters[comp_name] = shown + 1
+        if verbose:
+            print('\n')
+            pprint(post)
+            print('\n')
+        return post
+
+
+
+
+# HISTOGRAM
+
+# Fire update_histogram_meta callback 
+# every HISTOGRAM_PERIOD_SEC seconds
+HISTOGRAM_PERIOD_SEC = 20
+
+# For every company, 
+# how many tweets there were in a collection
+# before the callback fired
+histogram_counters = {
+    comp_name: db[comp_name].count_documents({})
+    for comp_name in comp_names
+}
+
+def update_histogram(histogram_counters, verbose=True):
+    if verbose:
+        print('========== Latest Tweets ==========')
+        print('Fetched latest tweets from mongo:')
+    distribution = {}
+
+    for comp_name, counter in histogram_counters.items():
+        
+        collection = db[comp_name]
+        actual_size = collection.count_documents({})
+        distribution[comp_name] = actual_size - counter        
+        if verbose:
+            print(
+                comp_name, 'has',
+                actual_size, 'tweets;',
+                'fetched:', actual_size - counter)
+    if verbose:
         print('\n')
-        pprint(doc)
-        shown1 += 1
-        latest_doc = doc
-        return doc
 
-    # finally:
-        # return doc
-        # time.sleep(1)
-
-
-
-
+    return distribution
---
+++ Display/layout.py
@@ -0,0 +1,32 @@
+
+import dash_html_components as html
+import dash_core_components as dcc
+
+
+def load_layout(history_period_sec, histogram_period_sec):
+    return html.Div(
+        html.Div([
+
+            html.H4('Live Twitter Data from MongoDB (1sec intervals)'),
+
+            # For history
+            html.Div(id='history-live-update-text'),
+            dcc.Interval(
+                id='history-interval-component',
+                interval=1000*history_period_sec,
+                n_intervals=0
+            ),
+
+            # For histogram
+            html.Div(id='histogram-live-update-text'),
+            dcc.Interval(
+                id='histogram-interval-component',
+                interval=1000*histogram_period_sec,
+                n_intervals=0
+            )
+
+
+        ])
+    )
+
+    
